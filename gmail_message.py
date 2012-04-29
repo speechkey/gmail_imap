4fe9fc93297f6df9b411ce4c2e529b2e5f84a055
Speechkey <gremoz@gmail.com>
2012-04-29 10:49:01 +0200

    Add separate handling of html and plain messages, PEP 8, custom search query

--- gmail_message.py
+++ gmail_message.py
@@ -1,23 +1,21 @@
-﻿import email,string
-import gmail_imap
+﻿
 
 class gmail_message:
-
     def __init__(self):
-    
         self.id = None
         self.uid = None
         self.flags = None
         self.mailbox = None
         self.date = None
-        
+        self.type = None
+        self.charset = None
+
         self.From = None
         self.Subject = '( no subject )'
         self.Body = None
-        
+        self.Plain_Body = None
+
     def __repr__(self):
-        str = "<gmail_message:  ID: %s  UID: %s Flags: %s Date: %s \n" % (self.id,self.uid,self.flags,self.date)
-        str += "from: %s subject: %s >" % (self.From,self.Subject)
+        str = "<gmail_message:  ID: %s  UID: %s Flags: %s Date: %s \n" % (self.id, self.uid, self.flags, self.date)
+        str += "from: %s subject: %s >" % (self.From, self.Subject)
         return str
-    
-    
