ffe4324f7749959dfd7f481a0eb7c85d503baf2b
speechkey <gremoz@gmail.com>
2012-05-02 20:52:39 +0200

    Recheck setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from distutils.core import setup
 setup(
     name='gmail_imap',
     version='1.0',
-    py_modules=['gmail_imap'],
+    py_modules=['gmail_imap', 'gmail_mailboxes', 'gmail_message', 'gmail_messages'],
 )
